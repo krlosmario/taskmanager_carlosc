b493a8f87a206597ade8a1e2bfe2a14bc3bdf1d9
krlosmario <c.mario19980000@gmail.com>
2021-07-06 20:55:33 -0500

    creando repo

---
+++ app/src/main/java/com/lsv/taskmanagercar/carlos/manager2.kt
@@ -0,0 +1,11 @@
+package com.lsv.taskmanagercar.carlos
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class manager2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manager2)
+    }
+}
